3e793e18c9a1fcfc034e6890c7226afa6991cb5f
Mason <park.mason.w@gmail.com>
2017-05-29 18:17:35 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/pokercalculator/Card.kt
@@ -0,0 +1,22 @@
+package com.example.pokercalculator
+
+/**
+ * Created by mwp20 on 5/28/2017.
+ */
+
+class Card {
+    private val mRank: Int = 0
+    private val mSuit: Int = 0
+
+    companion object {
+        private val JACK = 11
+        private val QUEEN = 12
+        private val KING = 13
+        private val ACE = 14
+        private val DIAMOND = 0
+        private val SPADE = 1
+        private val CLUB = 2
+        private val HEART = 3
+    }
+
+}
